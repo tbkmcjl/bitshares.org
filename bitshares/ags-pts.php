2b240cb785e7960cc67624b2d49db5816608a542
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-03-30 01:25:32 -0400

    adding ags-pts

---
+++ bitshares/ags-pts.php
@@ -0,0 +1,182 @@
+<?php $section_title = "AGS"; include '../header.php'; ?>
+<?php #echo "<script src=\"/assets/js/chart.js\"></script> "; ?>
+   <script src="/assets/js/pts.prices.js"></script>
+
+	<div id="white">
+		<div class="container">			
+<!--			<div class="inner-page row"> -->
+            <div class="row">
+               <div class="col-lg-3">
+               </div>
+               <div class="col-lg-8">
+               </div>
+            </div>
+				<div class="row">				
+					<div class="col-lg-6">					
+            <h1><span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> AGS</h1>
+            <p>
+            BitShares AGS is just an imaginary altcoin
+            with an extraordinary source of value. People who hold them get free shares in every new <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> company.
+            </p>
+
+            <h3>How does it work?</h3>
+            <p>
+            The only way you can possibly get AGS is to be a Patron Angel. 
+            You donated funds to help build and promote the <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> industry. People just don’t do that kind of thing without understanding the industry, and wanting to see it grow, and actually sacrificing to help it grow because they value shares in this industry and want to own more.
+            </p>
+            <p>
+               <center><b>DAC Developers Know This</b></center>
+            </p>
+            <p>
+            They know if they give you promotional shares, you won’t dump them.
+            You’ll take take a hard look at their business potential.
+            And you’ll probably buy or donate more – pushing up the value of their shares
+            And you’ll probably talk about them everywhere you go. 
+            </p>
+            <h3>How to become a Patron Angel</h3>
+            <p>
+            <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> AGS is a public way to document who has donated to help build the industry. 
+            Donations are used to help build software tools and a powerful ecosystem to help developers launch their new digital autonomous companies. It return, 3rd party 
+            developers may choose to give shares in their new companies as “tips” to those donors.  They do this primarily as a targeted promotion to attract the support 
+            of people who are likely to contribute to their new ventures in a variety of helpful ways.  Proven donors are Very Important Persons!
+            </p>
+							<h3>Support <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> with Bitcoin:</h3>					
+               <p style="text-align: justify;">								
+               </p>
+                 <a href="https://blockchain.info/address/1ANGELwQwWxMmbdaSWhWLqBEtPTkWb8uDc"><pre>1ANGELwQwWxMmbdaSWhWLqBEtPTkWb8uDc</pre></a>
+               <p>
+							<h3>Support <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> with PTS:</h3>					
+               </p>
+                 <a href="https://coinplorer.com/PTS/Addresses/PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw"><pre>PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw</pre></a>
+              <p>
+               <br/>
+               You can watch support flow in from the community at one of these independent 3rd party sites:<br/>
+               <p>
+               <a class="btn btn-yellow btn-lg" href="http://www1.agsexplorer.com">Visit www1.agsexplorer.com</a>
+               <br/>
+               We make no claim as to the accuracy of these sites as they are not maintained by us.  
+					 	 	  </p>
+               <h3 style="color:orange"><i>If you would like public recognition of your support, you must send all your support funds from a wallet for which you control the Private Key, such as Bitcoin-Qt.  This way you can prove to your friends that you are a supporter of Invictus.</i></h3>
+			   <p>Click <a href="/blockchain-info-ags-howto.php">here</a> to see how to send BTC from a blockchain.info wallet and then claim your BitShares AGS.<p> 
+               <h3 style="color:red">Do not send support directly from an exchange, if you care about getting public recognition of your support of our cause.  Otherwise that recognition will belong to the exchange.</h3>
+
+
+               <h3> What are the donations used for?</h3>
+              <p>
+               All BTC and PTS donations are put in a trust that will use funds in a manner that it feels will grow the <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> industry and secure the right to life, liberty and property for all mankind.  A public transaction ledger that outlines how we are allocating the funds given to the trust will be maintained. That said, these are no-strings-attached gifts and should not be construed to place any legal obligation on anyone to provide anything in return. You should only give if you believe this is a good cause and you trust us to use the funds wisely.
+               </p>
+               <p>
+               Gifts are not tax deductable and the <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> trust is not a registered non-profit organization.
+               </p>
+
+           </div> <!-- AGS COLUMN -->
+					<div class="col-lg-6">					
+            <h1><span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> PTS</h1>
+							<p style="text-align: justify;">
+              <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> PTS is a Bitcoin clone that allows you to mine and trade your support for the <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> ecosystem.  You become a
+              member of a larger community of individuals who believe in building decentralized technology when you own even a fraction of
+              a PTS.  
+							</p>
+              <p>
+              <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> PTS comes with a social consensus that at least 10% of the total shares in all future
+              <span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> blockchains should acknowledge the support given by the holders of PTS by providing an initial balance
+              in the genesis block of each new chain.  In the case of <a href="bitshares/x.php">bitshares X</a>
+              we recommend that supporters be acknowledged with 50% of the total shares in the genesis block.
+              </p>
+              <p>
+              We only recommend that those who choose to take our software and launch a bitshares blockchain recognize our supporters who made this possible.
+              <p>
+              <a class="btn btn-primary btn-lg" href="https://github.com/BitShares/BitShares-PTS/releases/download/v1.0.0/BitShares-PTS-1.0.0-win32-setup.exe">Download Windows Wallet (.exe)</a>
+              <br/>
+              <br/>
+              <a class="btn btn-primary btn-lg" href="https://github.com/BitShares/BitShares-PTS/releases/download/v1.0.0/BitShares-PTS-1.0.0-macosx.dmg">Download Mac Wallet (.dmg)</a>
+              <br/>
+              <br/>
+              <a class="btn btn-primary btn-lg" href="https://github.com/BitShares/BitShares-PTS">Source</a>
+              </p>
+              <p>
+              We have previously referred to BitShares PTS as "Protoshares", but no longer do so.  "ProtoShare" is a registered trademark of Site9, Inc. for its prototyping and mock-up software product, ProtoShare.  There is no relationship between ProtoShare and Invictus Innovations.
+              </p>
+
+              <div class="pts-resources">
+                    <h3> Mining Pools</h3>
+
+                      <p>Mining PTS on your own is hard. The network consists of many computers running calculations in the client software or special mining software. This keeps the network secure.
+                      But you can mine coins with your own processing power or cloud servers. The pool combines the power of all
+                      connected nodes and pays you proportionally for your share rate. Most pools have their own optimized miners, so do check out their "get started" pages.</p>
+
+                      <p>This is a list of pools mining PTS:</p>
+                      <ul style="list-style-type: circle;">
+                        <li><a href="http://ptsweb.beeeeer.org">http://ptsweb.beeeeer.org</a> - 2.5% fee, Pay Per Share</li>
+                        <li><a href="http://pts.rpool.net">http://pts.rpool.net</a> - 3% fee, payout per share of last round</li>
+                        <li><a href="http://ptspool.com">http://ptspool.com</a> - 3% fee, Pay Per Share </li>
+                        <li><a href="http://pts.1gh.com">http://pts.1gh.com</a> - 3.5% fee, Round-Based Pay Per Share (RBPPS)</li>
+                        <li><a href="http://ypool.net">http://ypool.net</a> - 5% fee, multiple coin pool, Pay Per Share</li>
+                      </ul>
+
+              </div><!-- end PTS Resources -->
+					<div >
+							<h3>Current Prices</h3>
+							<hr>
+
+							<ul id="exchange_list" class="nav nav-pills">
+							  <li class="active"><a href="#">Bter</a></li>
+							  <li ><a href="#">Cryptsy</a></li>
+							  <!-- Unsupported exchanges right now
+							  <li class="disabled" ><a href="#">Btc38</a></li>
+							  <li class="disabled" ><a href="#">Peatio</a></li>
+							  -->
+							</ul>
+							<table class="table table-bordered">
+								<tr>
+									<th>Price (USD)*</th>
+									<th>Price (BTC/PTS)</th>
+									<th>High (BTC/PTS)</th>
+									<th>Low (BTC/PTS)</th>
+									<th>Volume</th>
+								</tr>
+								<tr>
+									<td id="price-usd">0</td>
+									<td id="price-val">0</td>
+									<td id="high-val">0</td>
+									<td id="low-val">0</td>
+									<td id="vol-val">0</td>
+								</tr>
+							</table>
+							<small>* USD is calculated from Blockchain.info reported market price and Exchange PTS price.</small>
+
+              <h3> Exchanges</h3>
+
+                      <p>Exchanges are the trading websites where you can buy or sell PTS. Be careful not to place large sums of PTS on exchanges without adequate security measures, such as a good password and 2-factor authentication.</p>
+                      <p>If you hold PTS in an exchange rather than a wallet for which you control the Private Key, then those who launch new BitShares blockchains will be unable to recognize your support of the ecosystem. </p>
+                      <ul style="list-style-type: circle;">
+                          <li><a href="https://bter.com/">BTER Exchange</a></li>
+                          <li><a href="https://www.cryptsy.com/">Cryptsy Exchange</a></li>
+                          <li><a href="http://www.btc38.com/">BTC38 Exchange</a></li>
+                          <li><a href="https://peatio.com/">Peatio Exchange</a></li>
+                      </ul>
+
+                         <h3> Tools and Forum</h3>
+                      <p>To learn more about PTS, explore the following resources:</p>
+                      <ul style="list-style-type: circle;">
+                          <li><a href="https://coinplorer.com/PTS">PTS coin information - Current PTS statistics and transactions</a></li>
+                          <li><a href="https://bitsharestalk.org/index.php?board=1.0">Official PTS forum</a></li>
+                          <li><a href="http://www.reddit.com/r/protoshare">Sub-Reddit for PTS</a></li>
+                      </ul>
+						</div>
+
+				</div>
+
+			</div>
+
+
+    	</div><!-- /row -->
+          </div> <!-- PTS COLUMN -->
+
+
+    	</div><!-- /row -->
+			
+		
+	</div><!-- /white -->
+
+<?php include 'footer.php'; ?>
---
+++ bitshares/getprice.php
@@ -0,0 +1,32 @@
+<?php
+
+if($_REQUEST['exchange'] == 'Bter')
+{
+	$api_val = file_get_contents('https://bter.com/api/1/ticker/pts_btc');	
+	echo $api_val;
+}
+elseif ($_REQUEST['exchange'] == 'Cryptsy')
+{
+	$api_val = file_get_contents('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=119');	
+	echo $api_val;
+}
+elseif ($_REQUEST['exchange'] == 'Btc38')
+{
+
+}
+elseif ($_REQUEST['exchange'] == 'Peatio')
+{
+
+}
+elseif ($_REQUEST['exchange'] == 'MtGox')
+{
+	$api_val = file_get_contents('http://data.mtgox.com/api/2/BTCUSD/money/ticker');	
+	echo $api_val;	
+}
+elseif ($_REQUEST['exchange'] == 'blockchain')
+{
+	$api_val = file_get_contents('https://blockchain.info/ticker');	
+	echo $api_val;	
+}
+
+?>
