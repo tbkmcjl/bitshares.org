e212a42eb996ae2463ebd3aa9040fb58b4601348
tbkmcjl <tbkmcjl@163.com>
2014-04-02 06:19:31 +0800

    Create decentralized-autonomous-jedi-mind-tricks.php

---
+++ blog/decentralized-autonomous-jedi-mind-tricks.php
@@ -0,0 +1,190 @@
+<?php $section_title = "Decentralized Autonomous Jedi Mind Tricks"; include '../header.php'; ?>
+<div class="container">
+  <br/>
+  <br/>
+
+  <center><h1>Daniel Larimer & Charles Evans</h1></center>
+  <center><h3>March 30, 2014</h3></center>
+
+  <h2>Decentralized Autonomous Jedi Mind Tricks</h2>
+  <p>
+  In December David Johnston, one of the developers
+  of Mastercoin and co-founder of cryptocurrency 
+  angel investor network, BitAngels, published "
+  The Emerging Wave of Decentralized Applications"
+  on github, in which he describes Bitcoin and 
+  next-generation systems—specifically, BitShares,
+  Meshcoin, and Mastercoin—as 'Distributed Applications' 
+  as distinct from the original Distributed Autonomous 
+  Corporations (DAC). 
+  </p>
+
+  <p>
+  Johnston argues that this new phrase is preferable because
+  it avoids associations with regulated entities, like currency
+  and corporate shares, and because the thing being described is
+  a piece of open-source software that defines a protocol that 
+  can be modified only with the consensus of the community of 
+  programmers who maintain and extend the code base.
+  </p>
+
+  <p>
+  While avoiding misunderstandings caused by the literal interpretation 
+  of an analogy has merit, Johnston includes the issuance of tokens as
+  a defining characteristic of a Distributed Application, even though 
+  software applications, per se, do not require tokens. In other words,
+  DACs are a specific subset of Distributed Applications that involve
+  the creation of tokens—whether one calls them 'coins', 'shares',
+  'chips', or whatever—and to dispense with the DAC analogy is to call 
+  a subset by the name of its superset, rather like referring to pit bulls 
+  as 'canines', because pit bulls are regulated in some jurisdictions.
+  The choice of name does not change what a thing is, and animal control 
+  agents are unlikely to shrug and walk away satisfied if one points to 
+  a pack of pit bulls frolicking on one's front lawn and declares, "Oh 
+  those?  No, they're just canines. These aren't the pit bulls you're 
+  looking for."
+  </p>
+
+  <h2>A Meta-Analogy</h2>
+
+  <p>
+  Imagine you just discovered electricity and all of the amazing 
+  things that it can do.   There are electrons that flow around, 
+  wave forms, and other fundamental building blocks.   People want
+  to start using this amazing electricity concept to build useful
+  and productive circuits, but they lack a mental framework that
+  helps them intuitively understand and predict the outcome of a 
+  particular circuit structure, and even advanced mathematics can 
+  only approximate it due to noise in the environment and imperfect
+  measurements of the components.   Relying on math alone to simulate
+  the expected results is time-consuming.  To design systems you need 
+  intuition and abstraction.
+  </p>
+
+  <p>
+  So, someone comes along and says, "Wow! Electricity works just like 
+  plumbing!  It flows like water. There is a 'pressure' in the circuit. 
+  Resistors are like narrow pipes..."  If you think of an electrical  
+  circuit as a kind of electrical plumbing, then you can apply everything 
+  that you intuitively know about what works in plumbing systems to help 
+  design and understand this new fancy electricity thingamajig that we
+  just discovered, without having to resort to math until after a high-level
+  design is in place.  
+  </p>
+
+  <p>
+  Best of all, you quickly discover that teaching students about 
+  electricity is an order of magnitude easier using this metaphor,
+  and as a result they quickly pick up the concept and are able to
+  apply it in useful ways.
+  </p>
+
+  <p>
+  Now imagine that the plumbing industry is highly regulated and 
+  requires licenses.  Anything that moves a fluid through a pipe, 
+  stores a fluid in a tank, or employs a pressurized system is subject
+  to fees, regulations, and restrictions on its use. Plumbing might 
+  even be illegal in some places!
+  </p>
+
+  <p>
+  Members of the plumbing industry hear of this new metaphor for describing
+  electricity and want to regulate electricity with the same laws the apply 
+  to water and plumbing.  They might even want anyone who works with
+  electricity to go through the full training, apprenticeship, and guild 
+  membership process that applies to plumbers.
+  </p>
+
+  <p>
+  In order to avoid this kind of absurd situation, one must emphasize the 
+  distinction between what something is and the metaphor used to teach
+  how it operates.  Those using electricity don't care about how it works
+  or how it is classified. They just want the lights to come on.   
+  For this group you simply call it 'power' and nothing else is required. 
+  But, when you are trying to teach people how to use power to do things,
+  you need a metaphor.    
+  </p>
+
+  <p>
+  DAC is the metaphor that holds the key to designing the kinds of 
+  Distributed Applications Johnston describes.    Bitcoin is a 
+  token-issuing Distributed Application that is most easily understood 
+  using a currency metaphor.   BitShares is a token-issuing Distributed
+  Application most easily understood via a Bank, Corporation, or Exchange
+  metaphor.    
+  </p>
+
+  <p>
+  If we are afraid to use metaphors due to fear of regulators, then we 
+  will harm the ability of users to understand how and why token-issuing
+  Distributed Applications work in an intuitive level and also hinder
+  how they are designed.   One should never underestimate the power of 
+  the Dark Side. Regulators are not as easily distracted as Imperial
+  Stormtroopers. Renaming a DAC is neither necessary nor sufficient to
+  avoid attracting regulators' attention.  
+  </p>
+  
+  <p>
+  Many people have come up with DAC ideas, but it is clear that the 
+  first principles behind creating Distributed Autonomous Applications 
+  are not yet well understood.    The DAC metaphor is the key framework
+  that helps to design and implement token-issuing Distributed Autonomous 
+  Applications. 
+  </p>
+
+  <p>
+  Seen this way, Bitcoin is the first DAC. The service that it provides
+  that has attracted the greatest attention is payment services, and new 
+  uses are discovered with surprising subtlety and frequency.  
+  </p>
+
+  <p>
+  However, when seen in terms of shares rather than coins the tokens that
+  the Bitcoin system issues warrant closer scrutiny. In particular, note 
+  who in the Bitcoin system holds the voting power: the transaction
+  processors, known metaphorically as 'miners', and the holders of bitcoins
+  are passive participants. Miners control the majority of the computing 
+  power that makes up the Bitcoin system.  
+  </p>
+
+  <p>
+  Seen as a company, the holders of bitcoin-the-unit own non-voting shares.
+  The miners have all of the voting power.  
+  </p>
+
+  <p>
+  As a payment protocol, this obviously is not a devastating problem. 
+  Otherwise, Bitcoin would not exist. As a company, on the other hand, 
+  this is like having the vast majority of shares in circulation stripped 
+  of their voting rights, and all of the voting power concentrated in the
+  hands of a small number of parties who might not even hold the 
+  bitcoins—that they receive in payment for their services—longer than 
+  it takes to sell them. This situation is unusual in countries that tend 
+  to attract a lot of foreign investment, like Australia, Canada, the UK,
+  and the USA. It is much more like the large, family-owned combines that
+  one sees in the Developing World. 
+  </p>
+
+  <p>
+  Carrying the company analogy a bit further, the goal of maximizing the
+  'equity-per-share' ratio replaces the goal of maintaining a constant 
+  'backing-per-currency-unit' ratio in a currency system. With a currency,
+   the general tendency is to strive for a stable price-per-stuff ratio. 
+  Granted, one can argue in favor of a truly deflationary currency, but this
+  continues to be a minority view, and the default is stable prices for the 
+  vast majority of currency users. Shares, on the other hand, are expected 
+  to pay dividends, appreciate in value, or both. When we think of a DAC as
+  a company, rather than as a mint, the idea of units that increase in value
+  comes more naturally. 
+  </p>
+
+  <p>
+  If the conversation were about Distributed Applications, none of this might stand out as noteworthy. If the conversation is about DACs, these issues stand out like a Klingon at a Star Wars convention.
+  </p>
+  <center>Republished from <a href="http://ybitcoin.net">yBitcoin</a></center>
+  <br/>
+  <br/>
+
+</div>
+
+<?php include '../footer.php'?>
