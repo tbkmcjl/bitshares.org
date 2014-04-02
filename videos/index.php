fd8775104bcdeb7dc745d1f9fc78c6a77c6b9ce6
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-04-02 02:00:47 +0000

    adding videos

---
+++ videos/index.php
@@ -0,0 +1,41 @@
+<?php $section_title="Videos"; include '../header.php'?>
+<div id="white">
+  <div class="container">
+     <h1>Videos</h1>
+     <center>
+     <ul style="list-style:none">
+        <li>
+           <h2> What are bitshares? </h2>
+           <iframe src="//player.vimeo.com/video/89948761" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+           <iframe src="//player.vimeo.com/video/90419206" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+            <h2>bitshares - Texas Bitcoin Conference</h2>
+               <iframe src="//player.vimeo.com/video/89951386" width="480" height="270" frameborder="0" 
+                                  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+            <h2>Ethereum Q&A</h2>
+            <iframe src="//player.vimeo.com/video/86370915" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+            <h2>Discussion with Charles Evans & Daniel Larimer</h2>
+           <iframe src="//player.vimeo.com/video/88807515" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+            <h2> bitshares DNS </h2>
+            <iframe src="//player.vimeo.com/video/89219239" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+            <h2> Insurance (Mutual Aid Society) </h2>
+            <iframe src="//player.vimeo.com/video/88861284" width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </li>
+        <li>
+           <h2> Mining for World Domination </h2>
+           <iframe width="480" height="270" src="//www.youtube.com/embed/OaOVFoZkI_0" frameborder="0" allowfullscreen></iframe>
+        </li>
+     </ul>
+     </center>
+   </div>
+</div> 
+<?php  include '../footer.php'?>
